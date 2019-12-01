f390f0d8750b26cef166b159cb7594cf1e4aac93
Arsentiy Chander <arsentiy_ch@mail.ru>
2019-12-01 20:27:02 +0300

    nod algorithm

--- src/EuclidNOD.java
+++ src/EuclidNOD.java
@@ -1,3 +1,19 @@
 public class EuclidNOD {
 
+    public int nod(int a, int b) {
+        if (a == 0) {
+            return b;
+        } else if (b == 0) {
+            return a;
+        } else if (a > b) {
+            return nod(a % b, b);
+        } else {
+            return nod(b % a, a);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new EuclidNOD().nod(24,36));
+    }
+
 }
